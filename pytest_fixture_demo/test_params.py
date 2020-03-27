32961df2be809bd352c4fab9b5ae781e8c02c3c3
XFbeibei <1782343025@qq.com>
2020-03-27 23:44:22 +0800

    理解Xfail

--- pytest_fixture_demo/test_params.py
+++ pytest_fixture_demo/test_params.py
@@ -3,15 +3,19 @@
 import pytest
 
 
-@pytest.fixture(params=[1, 2, 3, 'linda'])
-def test_data(request):
-    return request.param
-
-
-# @pytest.fixture(params=[(1,3), (4,5)])   #params参数组合，执行失败
+# @pytest.fixture(params=[1, 2, 3, 'linda'])
 # def test_data(request):
 #     return request.param
 
 
+# @pytest.fixture(params=[(1,3), (4,5)])   #params输入的是列表是需要进行索引取值，
+# def test_data(request):
+#     return request.param
+
+@pytest.fixture(params=[{'a': 1, "b": 2}, {"name": 'Jerry', "age": 23}])  # params参数组合，执行失败
+def test_data(request):
+    print(request.param)
+    return request.param
+
 def test_one(test_data):
-    print('test_data1 :%s' % test_data)
+    print('test_data1 :%s' % test_data.values[0])
